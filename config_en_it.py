83078962fd4437248ac8bdd4faffdc294139c235
aap2239 <aap2239@columbia.edu>
2024-01-07 04:52:00 -0500

    black formatting

--- config_en_it.py
+++ config_en_it.py
@@ -8,14 +8,14 @@ def get_config():
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
-        "datasource": 'opus_books',
+        "datasource": "opus_books",
         "lang_src": "en",
         "lang_tgt": "it",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
         "tokenizer_file": "tokenizer_{0}.json",
-        "experiment_name": "runs/tmodel"
+        "experiment_name": "runs/tmodel",
     }
 
 
@@ -24,6 +24,7 @@ def get_weights_file_path(config, epoch: str):
     model_basename = config["model_basename"]
     model_filename = f"{model_basename}_{epoch}.pt"
     return str(Path(".") / model_folder / model_filename)
+
 
 def latest_weights_file_path(config):
     model_folder = config["model_folder"]
